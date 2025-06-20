297cf862e88b654735e692876ba1dc77059796b5
BrettFraley <brettfraley@proton.me>
2025-06-20 14:35:45 -0400

    Great progress - WIP - on text overaly and initial Scene Lab UI and code org

--- src/js/scenelab.js
+++ src/js/scenelab.js
@@ -3,30 +3,100 @@
 const DOM = {
     byId: id => document.getElementById(id),
     byClass: name => document.getElementsByClassName(name),
+
+    creatEl: (type, id, classes) => {
+        const el = document.createElement(type)
+        el.id = id
+        el.className = classes
+        return el
+    }
 }
 
 const defaultSceneOverlay = DOM.byId('default-text-overlay-wrapper')
-const testBrbButton = DOM.byId('brb-button')
+const newTextOverlay = DOM.byId('new-text-overlay')
 
-testBrbButton.addEventListener('click', () => {
-    scenLabTools.brb('test overlay... BRB')
+const testForm = DOM.byClass('text-overlay-form')[0]
+const createTextOverlayButton = DOM.byId('create-text-overlay-button')
+
+const activeTools = DOM.byId('active-tools')
+const createdTextOverlays = DOM.byId('created-text-overlays')
+
+
+// scratch me
+testForm.addEventListener('submit', e => e.preventDefault(), false)
+
+newTextOverlay.addEventListener('click', () => {
+    // This will call UIForms.overlays.textOverlay
+    const form = DOM.byClass('text-overlay-form')[0]
+    form.style.display = "inline-block"
 }, false)
 
-const scenLabTools = {
-
-    // Click New -> get create menu
-    // Click New Text Overlay
-    // NOTE: new user defined elements via tools get a generated DOM id
-        // textarea where you can just type text and choose element type
-        // or h1, h2, p, link, etc.
-        // color, font, custom css
+createTextOverlayButton.addEventListener('click', () => {
+    sceneLabTools.createTextOverlay()
+}, false)
 
 
-    // Click BRB <-- testing/dev
-    brb: text => {
-        const messageEl = document.createElement('h1')
-        messageEl.innerText = text
-        defaultSceneOverlay.appendChild(messageEl)
+
+const sceneLabTools = {
+
+    // New text overlay
+    createTextOverlay: () => {
+
+        const form = testForm
+        const id = Date.now().toString()
+
+        const name = form.elements.overlayName.value                
+        const type = form.elements.elementType.value
+        const text = form.elements.overlayText.value
+        const color = form.elements.color.value
+        const bgColor = form.elements.bgColor.value
+
+        const newOverlay = DOM.creatEl(type, id, `${id} text-overlay`)
+        newOverlay.innerText = text
+        newOverlay.style.color = color
+        newOverlay.style.backgroundColor = bgColor
+
+        // pos x, pos y in relation to bounding box of scene, z-index / ordering
+
+        defaultSceneOverlay.appendChild(newOverlay)
+
+        UIForms.createdPanel.addItem(id, name)
+        // activeTools.appendChild()
+
+
+    },
+
+
+}
+
+const UIForms = {
+
+    overlays: {
+
+        createTextOverlayForm: () => {
+            // <form class="tools-controls-ui-form text-overlay-form">
+            //             <label for="elementType">HTML Type</label>
+            //             <input name="elementType" type="text">
+
+            //             <label for="overlayText">Text</label>
+            //             <input name="overlayText" type="text">
+
+            //             <label for="color">Color</label>
+            //             <input name="color" type="color">
+            //             <button class="new-text-overlay-button">Create</button>
+            //         </form>
+
+        }
+    },
+
+    createdPanel: {
+        
+        addItem: (id, name) => {
+            const p = DOM.creatEl('p', id, "")
+            createdTextOverlays.appendChild(p)
+            p.innerText = name
+        }
+
     }
 
 }
